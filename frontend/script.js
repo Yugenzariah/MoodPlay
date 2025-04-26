3858ceb9b9ed2fa2e0cbdccae3cf25e5187826f1
Yugenzariah <yugenzariah@gmail.com>
2025-04-26 21:56:02 +1200

    Frontend started

---
+++ frontend/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('button[data-mood]').forEach(button => {
+    button.addEventListener('click', async () => {
+      const mood = button.getAttribute('data-mood');
+      const res = await fetch(`http://localhost:3000/playlist/${mood}`);
+      const data = await res.json();
+  
+      const resultDiv = document.getElementById('playlist-result');
+      resultDiv.innerHTML = `
+        <h2>${data.name}</h2>
+        <p>${data.description}</p>
+        <a href="${data.link}" target="_blank">🎵 Open Playlist</a>
+      `;
+    });
+});
